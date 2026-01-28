ea99c6ba13546e1f974aec7dae6a2efc10b7f554
Tomasz Trela <tomplus@gmail.com>
2026-01-28 12:15:42 +0100

    Change PowerShell script called in batch file

---
+++ restart_devices.bat
@@ -0,0 +1,21 @@
+@echo off
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+if '%errorlevel%' NEQ '0' (
+    echo I am requesting administrator privileges . . .
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%~dp0"
+
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "restart_devices.ps1"
+
+pause
