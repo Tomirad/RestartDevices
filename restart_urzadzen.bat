d394e58ba338b517dbcc6f9c501e2fbd9f969a32
Tomasz Trela <tomplus@gmail.com>
2026-01-28 12:14:50 +0100

    Add batch script to restart devices with admin rights

---
+++ restart_urzadzen.bat
@@ -0,0 +1,22 @@
+@echo off
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+if '%errorlevel%' NEQ '0' (
+    echo Prosze o uprawnienia administratora . . .
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%~dp0"
+
+:: TUTAJ URUCHAMIAMY POWERSHELL
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "restart_urzadzen.ps1"
+
+pause
